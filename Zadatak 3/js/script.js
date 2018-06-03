fcd134a52b0da8df283bb98d5dedebcb22094919
Vladimir Matovic <vladimir.matovic@blockpeek.com>
2018-06-03 12:31:50 +0200

    dodat zadatak 3

---
+++ Zadatak 3/js/script.js
@@ -0,0 +1,5 @@
+function sum () {
+    let n1 = document.getElementById("no.1").value;
+    let n2 = document.getElementById("no.2").value;
+    document.getElementById("result").value = +n1 + +n2;
+}
