0541a958a79b79d46f78168fd0da3cb5ad58da97
Vladimir Matovic <vladimir.matovic@blockpeek.com>
2018-06-03 14:13:47 +0200

    uradjen zadatak 5

--- Zadatak 5/zadatak5.js
+++ Zadatak 5/zadatak5.js
@@ -1,9 +1,34 @@
-let suma = 0;
+let numbers = [],
+    writeContainer;
 
 function add () {
     let number = document.getElementById("no.").value;
-    suma += +number;
+    writeContainer = document.getElementById("container");
 
-    document.getElementById("sum").value = suma;
+    numbers.push(+number);
+
+    // ako ima vise od jednog elementa u nizu, stavi zarez
+    if (numbers.length > 1){
+        writeContainer.innerHTML += (', ' + number);
+    } else {
+        writeContainer.innerHTML += (number);
+    }
+    
     document.getElementById("no.").value = '';
+}
+
+function sum () {
+    let sum = 0;
+
+    for (let n of numbers) {
+        sum += n;
+    }
+    document.getElementById("sum").value = sum;
+
+    return sum;
+}
+
+function avrg () {
+    let average  = sum ()/numbers.length;
+    document.getElementById("avrg").value = average;
 }
